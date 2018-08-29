259981fb972741e43d83e8538aa74d7060f69f85
Administrator <admin@whydah.local>
2018-08-29 16:50:59 -0400

    updated server core to display request message and output a response string

--- Sources/SwerverCore/SwerverCore.swift
+++ Sources/SwerverCore/SwerverCore.swift
@@ -5,23 +5,14 @@ import Socket
 public class EchoServer {
     
     public static let quitCommand: String = "QUIT"
-    public static let shutdownCommand: String = "SHUTDOWN"
     public static let bufferSize = Socket.SOCKET_DEFAULT_READ_BUFFER_SIZE
     
     public let port: Int
     public var listenSocket: Socket? = nil
     public var continueListening = true
-    public var connectedSockets = [Int32: Socket]()
     
     public init(port: Int) {
         self.port = port
-    }
-    
-    deinit {
-        for socket in connectedSockets.values {
-            socket.close()
-        }
-        self.listenSocket?.close()
     }
     
     public func start() throws {
@@ -29,51 +20,48 @@ public class EchoServer {
         
         listenSocket = socket
         try socket.listen(on: port)
-        print("Listening on port: \(socket.listeningPort)")
+        print("Listening on port: \(socket.listeningPort)\n")
         repeat {
             let connectedSocket = try socket.acceptClientConnection()
-            print("Connection from: \(connectedSocket.remoteHostname)")
+            print("Connection from: \(connectedSocket.remoteHostname)\n")
             newConnection(socket: connectedSocket)
             
         } while continueListening
     }
     
     private func newConnection(socket: Socket) {
-        connectedSockets[socket.socketfd] = socket
         
         var keepRunning = true
-        var readData = Data(capacity: EchoServer.bufferSize)
+        var readData: Data? = Data(capacity: EchoServer.bufferSize)
         
         do {
-            try socket.write(from: "Hi!, type 'QUIT' to end session or 'SHUTDOWN' to stop server.\n")
+            try socket.write(from: "Hi!, type 'QUIT' to end session and stop the server.\n")
             
             repeat {
-                let bytesData = try socket.read(into: &readData)
+                let bytesData = try socket.read(into: &readData!)
                 
                 if bytesData > 0 {
-                    guard let response = String(data: readData, encoding: .utf8) else {
+                    guard let request = String(data: readData!, encoding: .utf8) else {
                         print("Error decoding response...")
-                        readData.count = 0
+                        readData!.count = 0
                         break
                     }
-                    if response.hasPrefix(EchoServer.quitCommand) || response.hasPrefix(EchoServer.shutdownCommand) {
+                    print(request)
+                    print("Repsonse: 200 OK\n")
+                    
+                    if request.hasPrefix(EchoServer.quitCommand) {
                         keepRunning = false
                     }
+                    let reply = "Server says: \n\(request)\n"
+                    try socket.write(from: "\n\(reply)")
                     
-                    let reply = "Server response: \n\(response)\n"
-                    try socket.write(from: reply)
                 }
                 if bytesData == 0 {
                     keepRunning = false
                     break
                 }
-                
-                readData.count = 0
-                
+
             } while keepRunning
-            
-            print("Connection closed...")
-            socket.close()
         }
         catch let error {
             guard let socketError = error as? Socket.Error else {
@@ -85,10 +73,4 @@ public class EchoServer {
             }
         }
     }
-//
-//    private func shutdownServer() {
-//        print("Shutting down server...")
-//        continueListening = false
-//    }
-//
 }
