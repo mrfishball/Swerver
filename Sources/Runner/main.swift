ee7ae0e6b0f48e08011ea774d8776fe2e5b96a1c
Administrator <admin@whydah.local>
2018-08-29 16:50:09 -0400

    updated runner with instructions

--- Sources/Runner/main.swift
+++ Sources/Runner/main.swift
@@ -1,7 +1,7 @@
-//import Swerver
+let port = 5050
+let server = EchoServer(port: port)
+print("Echo Server")
+print("Connect with a browser by enter the URL 'localhost:\(port)'")
+print("Connect with a command line window by entering 'telnet ::1 \(port)'")
 
-let tool = EchoServer(port: 1337)
-
-do {
-    try tool.start()
-}
+try server.start()
