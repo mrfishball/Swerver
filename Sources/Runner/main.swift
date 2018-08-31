d612c612c07201dfc72de533634be5baecf4d62c
Administrator <admin@Whydah.local>
2018-08-31 09:20:46 -0400

    updated main run instructions

--- Sources/Runner/main.swift
+++ Sources/Runner/main.swift
@@ -2,6 +2,6 @@ let port = 5050
 let server = EchoServer(port: port)
 print("Echo Server")
 print("Connect with a browser by enter the URL 'localhost:\(port)'")
-print("Connect with a command line window by entering 'telnet ::1 \(port)'")
+print("Connect with a command line window by entering 'telnet localhost \(port)'")
 
 try server.start()
