3685236af798e7f5d1740636ef1bf8f8f23f0724
mrfishball <mrfishball@gmail.com>
2018-09-06 21:05:31 -0400

    updated travis and install script for CI deployment

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# Automatically installs swiftenv and run's swiftenv install.
-# This script was designed for usage in CI systems.
+# Automatically installs clang and swift snapshot.
+# This script was designed for usage in Travis CI.
 
+sudo apt-get update
 sudo apt-get install clang libicu-dev
 mkdir swift
-curl https://swift.org/builds/$SWIFT_BRANCH/ubuntu1404/$SWIFT_VERSION/$SWIFT_VERSION-ubuntu14.04.tar.gz -s | tar xz -C swift &> /dev/null
-export PATH="$(pwd)/swift/$SWIFT_VERSION-ubuntu14.04/usr/bin:$PATH"
+curl https://swift.org/builds/$SWIFT_BRANCH/ubuntu1610/$SWIFT_VERSION/$SWIFT_VERSION-ubuntu16.10.tar.gz -s | tar xz -C swift &> /dev/null
+export PATH="$(pwd)/swift/$SWIFT_VERSION-ubuntu16.10/usr/bin:$PATH"
