1751efbe2b419a3a6d6676026d19d22d9d948266
mrfishball <mrfishball@gmail.com>
2018-09-10 10:57:44 -0400

    added first test

--- Tests/SwerverTests/SwerverTests.swift
+++ Tests/SwerverTests/SwerverTests.swift
@@ -1,1 +1,17 @@
+import Quick
+import Nimble
+import Swerver
 
+class SwerverCoreTests: QuickSpec {
+    override func spec() {
+        describe("An HTTP server") {
+            let server = Swerver(port: 5050)
+            
+            context("when the server is running") {
+                it("must be able to accept a connection") {
+                    expect(server.connected).to(equal(true))
+                }
+            }
+        }
+    }
+}
