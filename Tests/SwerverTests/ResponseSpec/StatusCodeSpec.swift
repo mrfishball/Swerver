23829a813ad9b0572e424188777cfe4a84d2b0f1
mrfishball <mrfishball@gmail.com>
2018-10-16 13:43:30 -0400

    refactor specs

--- Tests/SwerverTests/ResponseSpec/StatusCodeSpec.swift
+++ Tests/SwerverTests/ResponseSpec/StatusCodeSpec.swift
@@ -4,17 +4,17 @@ import Swerver
 
 class StatusCodeSpec: QuickSpec {
     override func spec() {
-        describe("A RequestStatus enum") {
+        describe("A StatusCode enum") {
             
-            it("can return a status phrase of 'OK'") {
+            it("returns a status phrase of 'OK'") {
                 expect(StatusCode.ok.getStatusPhrase()).to(equal("OK"))
             }
             
-            it("can return a status phrase of 'Not Found'") {
+            it("returns a status phrase of 'Not Found'") {
                 expect(StatusCode.not_found.getStatusPhrase()).to(equal("Not Found"))
             }
             
-            it("can return a status phrase of 'Method Not Allowed'") {
+            it("returns a status phrase of 'Method Not Allowed'") {
                 expect(StatusCode.not_allowed.getStatusPhrase()).to(equal("Method Not Allowed"))
             }
         }
